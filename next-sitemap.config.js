97a9ee4fa4c00ab3350244eb3dd3bd99fb23a900
anyoshe <anyokaayoka@gmail.com>
2025-06-22 16:00:49 +0300

    added site maps

---
+++ next-sitemap.config.js
@@ -0,0 +1,7 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://getaxe.tech', // ✅ use your full domain
+  generateRobotsTxt: true,        // ✅ generates robots.txt
+  sitemapSize: 5000,
+};
+
