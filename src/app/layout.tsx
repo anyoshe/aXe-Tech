bd8efc09a8614237321374147ba700ee58290213
anyoshe <anyokaayoka@gmail.com>
2025-07-20 15:13:35 +0300

    tracking

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -99,6 +99,37 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+         {/* Meta Pixel Code */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              !function(f,b,e,v,n,t,s)
+              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+              n.queue=[];t=b.createElement(e);t.async=!0;
+              t.src=v;s=b.getElementsByTagName(e)[0];
+              s.parentNode.insertBefore(t,s)}(window, document,'script',
+              'https://connect.facebook.net/en_US/fbevents.js');
+              fbq('init', '673031319116971'); 
+              fbq('track', 'PageView');
+            `,
+          }}
+        />
+        <noscript>
+          <img height="1" width="1" style={{ display: "none" }}
+            src="https://www.facebook.com/tr?id=673031319116971&ev=PageView&noscript=1" />
+        </noscript>
+        {/* End Meta Pixel Code */}
+        {/* ✅ HubSpot Tracking Code */}
+        <script
+          async
+          defer
+          id="hs-script-loader"
+          src="//js-eu1.hs-scripts.com/146581672.js"
+        ></script>
+      </head>
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased bg-black text-white`}>
         {children}
         <SocialNavbar /> {/* 👈 Sticky Social Icons visible on every page */}
